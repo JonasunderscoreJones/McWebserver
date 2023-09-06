10c3aa2048f52add9b825882a385d758e3cf9317
Jonas_Jones <91549607+J-onasJones@users.noreply.github.com>
2023-09-06 20:47:54 +0200

    fix unnecessary verbose logger while server stop

--- src/main/java/me/jonasjones/mcwebserver/mixin/WebserverStopMixin.java
+++ src/main/java/me/jonasjones/mcwebserver/mixin/WebserverStopMixin.java
@@ -1,6 +1,7 @@
 package me.jonasjones.mcwebserver.mixin;
 
 import me.jonasjones.mcwebserver.McWebserver;
+import me.jonasjones.mcwebserver.config.ModConfigs;
 import me.jonasjones.mcwebserver.web.ServerHandler;
 import net.minecraft.server.MinecraftServer;
 import org.spongepowered.asm.mixin.Mixin;
@@ -13,6 +14,7 @@ public class WebserverStopMixin {
 	@Inject(at = @At("HEAD"), method = "shutdown")
 	private void init(CallbackInfo info) {
 		McWebserver.LOGGER.info("Stopping Webserver...");
+		ModConfigs.VERBOSE = false;
 		ServerHandler.mcserveractive = false;
 	}
 }
